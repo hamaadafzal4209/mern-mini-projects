9c4553ae9eb662af6cd0da8bccf298f29e40d25c
FrontendFlair <hamaadafzal404@gmail.com>
2024-07-15 07:13:14 -0700

    create login route

--- CRUD APP/api/Routes/userRoute.js
+++ CRUD APP/api/Routes/userRoute.js
@@ -1,7 +1,8 @@
-import express from 'express';
-import { register } from '../controllers/userController.js';
+import express from "express";
+import { login, register } from "../controllers/userController.js";
 const router = express.Router();
 
-router.post("/register" , register)
+router.post("/login", login);
+router.post("/register", register);
 
 export default router;
--- CRUD APP/api/controllers/userController.js
+++ CRUD APP/api/controllers/userController.js
@@ -1,5 +1,6 @@
 import userModel from "../model/userModel.js";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 export const register = async (req, res) => {
   const { name, email, password } = req.body;
@@ -20,7 +21,31 @@ export const register = async (req, res) => {
     });
 
     const user = await newUser.save();
-    res.json({ success: true, message: "User created successfully",user });
+    res.json({ success: true, message: "User created successfully", user });
+  } catch (error) {
+    console.log(error);
+    return res.json({ success: false, message: error.message });
+  }
+};
+
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await userModel.findOne({email});
+
+    if (!user) {
+      return res.json({ success: false, message: "User not found" });
+    }
+
+    const isMatch = bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.json({ success: false, message: "Wrong Cradientials" });
+    }
+
+    const token = jwt.sign({ id: user.id }, "jkdjdiejnnjdn");
+    res.json({ success: true, message: "Login successful" });
   } catch (error) {
     console.log(error);
     return res.json({ success: false, message: error.message });
